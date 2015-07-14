8ed9cbfbbc7549f3dc5ec7c041ca2652b83ce319
Kirill Kliavin <kirill@studiothick.com>
2015-07-14 11:16:04 +1000

    pull artwork from file

--- crawler.php
+++ crawler.php
@@ -1,4 +1,5 @@
 <?php 
+
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
@@ -12,21 +13,17 @@ $albumAnchor = "TALB";
 $artisAnchor = "TPE1";
 $titleAnchor = "TIT2";
 
-$artworkAnchor = "artworkURL_640x";
-$artworkAnchorEnd = ".jpg";
-
 function stripRandomChars($str){
 	$str = trim($str);
 	$chars = str_split($str);
-$flag = false;
+
 	$start = 0;
 	$finish = count($chars);	
-	for ($i=0; $i < count($chars); $i++) { 
-		if(!$flag && ord($chars[$i]) > 32 && $chars[$i] < 128){
+	for ($i=1; $i < count($chars); $i++) { 
+		if(!$start && ord($chars[$i]) > 32 && $chars[$i] < 128){
 			$start = $i;
-$flag= true;
 		}
-		if($flag && (ord($chars[$i]) < 32 || ord($chars[$i]) >= 128)){
+		if($start && (ord($chars[$i]) < 32 || ord($chars[$i]) >= 128)){
 			$finish = $i;
 			break;
 		}
@@ -37,48 +34,60 @@ $flag= true;
 $playlist = file_get_contents($URL . 'prog.m3u8');
 
 $pos = strrpos($playlist, ",");
+
 $fileName = trim(substr($playlist, $pos + 2));//get last file
+
+
 $lastFileContent = file_get_contents("lastFile.txt");
+
+
 if($fileName == $lastFileContent){
 	echo "file duplicate";
 	return;
 }
+
 $lastFile = fopen("lastFile.txt", "w");
 fwrite($lastFile, $fileName);
 fclose($lastFile);
 
- $file = file_get_contents($URL . $fileName, false,NULL,-1, 3000);
+
+
+//echo $fileName . "\r\n";
+
+
+$file = file_get_contents($URL . $fileName, false,NULL,-1, 3000);
+
+
+//echo $file. "<br>";
+
 
 $albumPos = strpos($file, $albumAnchor);
 $artistPos = strpos($file,$artisAnchor);
 $titlePos = strpos($file, $titleAnchor);
 
-$artworkStart = strpos($file, $artworkAnchor); 
-$artworkEnd = strpos($file, $artworkAnchorEnd, 
-$artworkStart);
 
-if($artworkStart && $artworkEnd){
-$artwork =  stripRandomChars(substr($file, 
-$artworkStart + strlen($artworkAnchor), 
-$artworkEnd + strlen($artworkAnchorEnd) - 
-$artworkStart-strlen($artworkAnchor)));
-echo "Artwork URL:". $artwork . "<br>";
-}
 if($titlePos){
 	$title =  stripRandomChars(substr($file, $titlePos + strlen($titleAnchor), 100));
-	echo "Title:".$title . "<br>";
+	echo $title . "<br>";
 }
- if($albumPos && $artistPos){
+
+
+if($albumPos && $artistPos){
 	$album =stripRandomChars(substr($file, $albumPos + strlen($albumAnchor), $artistPos - $albumPos-strlen($albumAnchor)));	
-	echo "Album:".$album . "<br>";
+	echo $album . "<br>";
 }
+
 if($artistPos && $titlePos){
 	$artist =stripRandomChars(substr($file, $artistPos + strlen($artisAnchor), $titlePos - $artistPos-strlen($artisAnchor)));	
-	echo "Artist:".$artist . "<br>";
+	echo $artist . "<br>";
 }else{
 	echo "not inough info";
 	return;
 }
+
+
+
+
 $lastSongContent = file_get_contents("lastSong.txt");
 if($title  == $lastSongContent){
 	echo "song duplicate";
@@ -87,7 +96,23 @@ if($title  == $lastSongContent){
 $lastSong = fopen("lastSong.txt", "w");
 fwrite($lastSong, $title);
 fclose($lastSong);
-$content = "<div style='background-image: url({$artwork});'><h1>{$artist} - {$title}</h1></div>";
+
+
+$term = urlencode($artist . (isset($album)?(" " . $album):"") . " " . $title);
+//echo $term;
+$iTunesJSON =  file_get_contents('http://itunes.apple.com/search?term='.$term.'&media=music&entity=song');
+$iTunesData  = json_decode($iTunesJSON, true);
+
+if($iTunesData["resultCount"] == 0){
+	echo "no itunes info";
+	return;
+}
+
+$iTunesMetadata = $iTunesData["results"][0];
+$iTunesMetadata["artworkUrl1500"] = str_replace(".100x100-75", ".1500x1500-75", $iTunesMetadata["artworkUrl100"]);
+$content = "<div style='background-image: url({$iTunesMetadata["artworkUrl1500"]});'><h1>{$iTunesMetadata["artistName"]} - {$iTunesMetadata["trackName"]}</h1></div>";
+
+
 $contentFile = fopen("content.php", "w");
 fwrite($contentFile, $content);
 fclose($contentFile);
