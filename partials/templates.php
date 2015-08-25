811a359a80a7aa61239263cbc4242bf467600fc2
Kirill Kliavin <kirill@studiothick.com>
2015-08-25 22:37:28 +1000

    table tweak

--- partials/templates.php
+++ partials/templates.php
@@ -4,10 +4,9 @@ $templates = array(
 'now' => '<div id="now" style="background-image: url({{artworkUrl}})"><h1>{{artistName}} &mdash; {{trackName}}</h1>',
 'topgrid' => '<div id="grid"><!--
 				{{# tracks }}
-					--><div class="show-track grid-item{{className}}" data-id="{{trackId}}" data-src="{{artworkUrl100}}" data-plays="{{plays}}" data-target-id="{{unique_tile}}">
-						<a class="preview" target="_blank" href=" {{trackViewUrl}} " title="{{title}}{{trackName}}  &mdash; {{artistName}}" >
+					--><div class="show-track grid-item" data-id="{{trackId}}" data-src="{{artworkUrl100}}" data-plays="{{plays}}" data-target-id="{{unique_tile}}">
+						<a class="preview" target="_blank" href=" {{trackViewUrl}} " title="{{trackName}}  &mdash; {{artistName}}" >
 							<img class="artwork" src= "{{artworkUrl100}}"  alt="{{trackName}} &mdash; {{artistName}}" />
-							{{# className }}<div class="badge"></div>{{/ className }}
 						</a>
 					</div><!--
 				{{/ tracks }}
