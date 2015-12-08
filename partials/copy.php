ff58998f8d1a16c7b8363007a3704b5524617b5d
Kirill Kliavin <k.kliavin@gmail.com>
2015-12-08 21:51:42 +1100

    copy

--- partials/copy.php
+++ partials/copy.php
@@ -18,5 +18,5 @@ $i = 0;
 }?>
 </span>
 <!-- <span class="links"><a href="/" title="Recent Hits on Beats 1">Recent Hits</a> | <a href="/top100" title="Top 100 Tracks on Beats 1">Top 100</a> | <a href="/now" title="Now playing on Beats 1">Now playing</a></span> -->
-<span class="info">&copy;<?php echo date("Y") . " " .$siteName; ?> by <a href="https://twitter.com/KaKaUandME" title="Kirill Kliavin on Twitter" target="_blank">@kakauandme</a></span>
+<span class="info">&copy; <?php echo date("Y") . " " .$siteName; ?> by <a href="https://github.com/kakauandme/beats1.media#beats1media" title="View on Github" target="_blank">@kakauandme</a></span>
 </p>
